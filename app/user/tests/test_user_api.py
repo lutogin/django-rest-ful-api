2a3c776db47b01637e5540b34138d97f405e9abf
Andrey Lutogin <lutogin@trueconf.ru>
2019-08-14 22:31:11 +0300

    Add user endpoint add tests

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,6 +7,8 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**param):
@@ -54,3 +56,81 @@ class PublicUserApiTests(TestCase):
         ).exists()
 
         self.assertFalse(user_exist)
+
+    def test_create_token_for_user(self):
+        """Test that a token is created for the user"""
+        payload = {'email': 'test@test.tt', 'password': 'Qwerty12345'}
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """Test tha token is not created credentials are given"""
+        create_user(email='test@test.tt', password='Qwerty12345')
+        payload = {'email': 'test@test.tt', 'password': 'wrong'}
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_no_user(self):
+        """Test that token is not created if user doens't exist"""
+        payload = {'email': 'test@londonappdev.com', 'password': 'testpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_missing_field(self):
+        """Test that email and password are required"""
+        res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    class PrivateUserApiTests(TestCase):
+        """Test API requests that require authentication"""
+
+        def setUp(self):
+            self.user = create_user(
+                email='test@londonappdev.com',
+                password='testpass',
+                name='fname',
+            )
+            self.client = APIClient()
+            self.client.force_authenticate(user=self.user)
+
+        def test_retrieve_profile_success(self):
+            """Test retrieving profile for logged in user"""
+            res = self.client.get(ME_URL)
+
+            self.assertEqual(res.status_code, status.HTTP_200_OK)
+            self.assertEqual(res.data, {
+                'name': self.user.name,
+                'email': self.user.email,
+            })
+
+        def test_post_me_not_allowed(self):
+            """Test that POST is not allowed on the me URL"""
+            res = self.client.post(ME_URL, {})
+
+            self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        def test_update_user_profile(self):
+            """Test updating the user profile for authenticated user"""
+            payload = {'name': 'new name', 'password': 'newpassword123'}
+
+            res = self.client.patch(ME_URL, payload)
+
+            self.user.refresh_from_db()
+            self.assertEqual(self.user.name, payload['name'])
+            self.assertTrue(self.user.check_password(payload['password']))
+            self.assertEqual(res.status_code, status.HTTP_200_OK)
